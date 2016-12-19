0bb881721789df6949a9a583b4e51f8c5175795d
Lokesh Devnani <lokeshthegenius@gmail.com>
2016-12-20 01:36:03 +0530

    Python Error Case displayed with error dialog

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ try:
     f.write(pyperclip.paste())
     f.close()
 
-    os.system("python tempInput.txt > tempOutput.txt")
-    os.system("x=$(cat tempOutput.txt); zenity --info --text=\"$x\"");
+    status = os.system("python tempInput.txt > tempOutput.txt")
+    if status:
+        os.system("x=$(cat tempOutput.txt); zenity --error --text=\"Some error Occurred: $x\"");
+    else:
+        os.system("x=$(cat tempOutput.txt); zenity --info --text=\"$x\"");
 except:
     print('Something went wrong! Can\'t tell what?')
     sys.exit(0) # quit Python
